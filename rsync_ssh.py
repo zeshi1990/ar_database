02ec81b6245c889d7922b58c44c3685c35a497ff
zeshi.z@gmail.com <zeshi@glasercompserver-X9DR3-F>
2016-03-28 21:13:36 -0700

    Added timeout handling

--- rsync_ssh.py
+++ rsync_ssh.py
@@ -11,7 +11,10 @@ with open("/media/raid0/zeshi/AR_db/passphrase.txt") as f:
     
 # Rsync everything
 child = pexpect.spawn("rsync -avz -e ssh ziran@192.168.1.100:~/AR2014/data /media/raid0/zeshi/AR_db/tmp")
-child.expect("Enter passphrase for key '/home/zeshi/.ssh/id_rsa':")
-child.sendline(my_pass)
+try:
+    child.expect("Enter passphrase for key '/home/zeshi/.ssh/id_rsa':")
+    child.sendline(my_pass)
+except pexpect.TIMEOUT as timeout:
+    print(timeout)
 child.interact()
 
