d74b6d327b96cfad29ccb889c7392161f631fbb9
zeshi.z@gmail.com <zeshi@glasercompserver-X9DR3-F>
2016-04-09 09:32:44 -0700

    Changed

--- rsync_ssh.py
+++ rsync_ssh.py
@@ -3,20 +3,5 @@
 
 # In[ ]:
 
-# import pexpect
-
-# # Read my password for ssh key
-# with open("/media/raid0/zeshi/AR_db/passphrase.txt") as f:
-#     my_pass = f.readline()
-    
-# # Rsync everything
-# child = pexpect.spawn("rsync -avz -e ssh ziran@192.168.1.100:~/AR2014/data /media/raid0/zeshi/AR_db/tmp")
-# try:
-#     child.expect("Enter passphrase for key '/home/zeshi/.ssh/id_rsa':")
-#     child.sendline(my_pass)
-# except pexpect.TIMEOUT as timeout:
-#     print(timeout)
-# child.interact()
-
 import os
 os.system("rsync -avz -e ssh ziran@glaser.berkeley.edu:~/AR2014/data /media/raid0/zeshi/AR_db/tmp")
